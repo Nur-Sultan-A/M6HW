2b2cb5ad6eb3c925c1ff8284d7846893c39bbe70
arnursultan <badasspubgm@gmail.com>
2026-04-14 00:59:14 +0600

    add Redis, Celery

--- config/settings.py
+++ config/settings.py
@@ -115,3 +115,23 @@ CACHES = {
 }
 
 CACHE_TTL = 60 * 15
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_TIMEZONE = "Asia/Bishkek"
+
+from celery.schedules import crontab
+
+CELERY_BEAT_SCHEDULE = {
+    "cleanup-blacklisted-tokens": {
+        "task": "users.tasks.cleanup_blacklisted_tokens",
+        "schedule": crontab(hour=0, minute=0),
+    },
+    "refresh-users-cache": {
+        "task": "users.tasks.refresh.users_cache",
+        "schedule": crontab(minute="*10"),
+    },
+}
---
+++ users/tasks.py
@@ -0,0 +1,51 @@
+from celery import shared_task
+from django.core.cache import cache
+from django.utils import timezone
+from datetime import timedelta
+
+from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken
+
+
+@shared_task
+def send_welcome_email(user_id: int):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+
+    try:
+        user = User.objects.get(pk=user_id)
+        print(f"Отправляем письмо на {user.email}")
+        return f"Письмо отправлено на {user.email}"
+    except User.DoesNotExist:
+        return f"Пользователь {user_id} не найден"
+
+@shared_task
+def deactivate_inactive_users():
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+
+    deadline = timezone.now() - timedelta(days=30)
+    users = User.objects.filter(
+        last_login__lt=deadline,
+        is_active=True,
+        role="user"
+    )
+    count = users.update(is_active=False)
+    return f"Деактивировано {count} пользователей"
+
+@shared_task
+def cleanup_blacklisted_tokens():
+    from rest_framework_simplejwt.token_blacklist.models import (
+        OutstandingToken,
+        BlacklistedToken
+    )
+    expired = OutstandingToken.objects.filter(
+        expires_at__lt=timezone.now()
+    )
+    count = expired.count()
+    expired.delete()
+    return f"Удалено {count} истёкших токенов"
+
+@shared_task
+def refresh_users_cache():
+    cache.delete("users_list")
+    return "Кэш users_list сброшен"
